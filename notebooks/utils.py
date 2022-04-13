7757e83fdfe89646e1281458a1dbd302de1c7b12
Paul Iusztin <p.e.iusztin@gmail.com>
2022-04-13 14:05:35 +0300

    Finish TODOs.

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -3,6 +3,7 @@ from typing import Tuple
 
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
 
 def load_data(path_dir: str = "../data") -> Tuple[pd.DataFrame, pd.DataFrame]:
@@ -23,3 +24,74 @@ def load_data(path_dir: str = "../data") -> Tuple[pd.DataFrame, pd.DataFrame]:
     test_df["item_cnt_month"] = np.nan
 
     return train_df, test_df
+
+
+def clean(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    :param df: dataframe that will have its NaN values filled, useless columns dropped and outliers removed.
+    :return: cleaned dataframe
+    """
+
+    return df
+
+
+def add_features(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    :param df: dataframe that will have its features added.
+    :return: dataframe with added features
+    """
+
+    df = _add_city_features(df)
+
+    return df
+
+
+def _add_city_features(df: pd.DataFrame) -> pd.DataFrame:
+    df['city_name'] = df['shop_name'].apply(lambda x: x.split()[0].lower())
+    df.loc[df.city_name == '!якутск', 'city_name'] = 'якутск'
+    df['city_id'] = LabelEncoder().fit_transform(df['city_name'])
+
+    coords = dict()
+    coords['якутск'] = (62.028098, 129.732555, 4)
+    coords['адыгея'] = (44.609764, 40.100516, 3)
+    coords['балашиха'] = (55.8094500, 37.9580600, 1)
+    coords['волжский'] = (53.4305800, 50.1190000, 3)
+    coords['вологда'] = (59.2239000, 39.8839800, 2)
+    coords['воронеж'] = (51.6720400, 39.1843000, 3)
+    coords['выездная'] = (0, 0, 0)
+    coords['жуковский'] = (55.5952800, 38.1202800, 1)
+    coords['интернет-магазин'] = (0, 0, 0)
+    coords['казань'] = (55.7887400, 49.1221400, 4)
+    coords['калуга'] = (54.5293000, 36.2754200, 4)
+    coords['коломна'] = (55.0794400, 38.7783300, 4)
+    coords['красноярск'] = (56.0183900, 92.8671700, 4)
+    coords['курск'] = (51.7373300, 36.1873500, 3)
+    coords['москва'] = (55.7522200, 37.6155600, 1)
+    coords['мытищи'] = (55.9116300, 37.7307600, 1)
+    coords['н.новгород'] = (56.3286700, 44.0020500, 4)
+    coords['новосибирск'] = (55.0415000, 82.9346000, 4)
+    coords['омск'] = (54.9924400, 73.3685900, 4)
+    coords['ростовнадону'] = (47.2313500, 39.7232800, 3)
+    coords['спб'] = (59.9386300, 30.3141300, 2)
+    coords['самара'] = (53.2000700, 50.1500000, 4)
+    coords['сергиев'] = (56.3000000, 38.1333300, 4)
+    coords['сургут'] = (61.2500000, 73.4166700, 4)
+    coords['томск'] = (56.4977100, 84.9743700, 4)
+    coords['тюмень'] = (57.1522200, 65.5272200, 4)
+    coords['уфа'] = (54.7430600, 55.9677900, 4)
+    coords['химки'] = (55.8970400, 37.4296900, 1)
+    coords['цифровой'] = (0, 0, 0)
+    coords['чехов'] = (55.1477000, 37.4772800, 4)
+    coords['ярославль'] = (57.6298700, 39.8736800, 2)
+    df['city_coord_1'] = df['city_name'].apply(lambda x: coords[x][0])
+    df['city_coord_2'] = df['city_name'].apply(lambda x: coords[x][1])
+    df['country_part'] = df['city_name'].apply(lambda x: coords[x][2])
+
+    return df
+
+
+if __name__ == "__main__":
+    train_df, _ = load_data()
+    train_df = clean(train_df)
+    train_df = add_features(train_df)
+    print(train_df)
