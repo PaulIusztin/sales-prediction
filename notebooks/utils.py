921b51a6001a7127c85c547a0536991532204e23
Paul Iusztin <p.e.iusztin@gmail.com>
2022-04-14 16:57:08 +0300

    Add new item features. Solve some todos.

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -41,51 +41,76 @@ def add_features(df: pd.DataFrame) -> pd.DataFrame:
     :return: dataframe with added features
     """
 
-    df = _add_city_features(df)
+    feature_to_add_func = [_add_is_new_item_feature, _add_first_shop_transaction_feature, _add_city_features]
+    for func in feature_to_add_func:
+        df = func(df)
 
     return df
 
 
+def _add_is_new_item_feature(df: pd.DataFrame) -> pd.DataFrame:
+    is_new_items_df = df.groupby(["item_id"])["date"].min().reset_index()
+    is_new_items_df["is_new_item"] = 1
+    df = pd.merge(df, is_new_items_df[["date", "item_id", "is_new_item"]],
+                  on=["date", "item_id"], how="left")
+    df["is_new_item"] = df["is_new_item"].fillna(0)
+    df["is_new_item"] = df["is_new_item"].astype("int8")
+
+    return df
+
+
+def _add_first_shop_transaction_feature(df: pd.DataFrame) -> pd.DataFrame:
+    is_first_shop_transaction_df = df.groupby(["shop_id", "item_id"])["date"].min().reset_index()
+    is_first_shop_transaction_df["is_first_shop_transaction"] = 1
+    df = pd.merge(df, is_first_shop_transaction_df[["date", "shop_id", "item_id", "is_first_shop_transaction"]],
+                  on=["date", "shop_id", "item_id"], how="left")
+    df["is_first_shop_transaction"] = df["is_first_shop_transaction"].fillna(0)
+    df["is_first_shop_transaction"] = df["is_first_shop_transaction"].astype("int8")
+    
+    return df
+
+
 def _add_city_features(df: pd.DataFrame) -> pd.DataFrame:
-    df['city_name'] = df['shop_name'].apply(lambda x: x.split()[0].lower())
-    df.loc[df.city_name == '!якутск', 'city_name'] = 'якутск'
-    df['city_id'] = LabelEncoder().fit_transform(df['city_name'])
+    df["city_name"] = df["shop_name"].apply(lambda x: x.split()[0].lower())
+    df.loc[df.city_name == "!якутск", "city_name"] = "якутск"
+    df["city_id"] = LabelEncoder().fit_transform(df["city_name"])
 
     coords = dict()
-    coords['якутск'] = (62.028098, 129.732555, 4)
-    coords['адыгея'] = (44.609764, 40.100516, 3)
-    coords['балашиха'] = (55.8094500, 37.9580600, 1)
-    coords['волжский'] = (53.4305800, 50.1190000, 3)
-    coords['вологда'] = (59.2239000, 39.8839800, 2)
-    coords['воронеж'] = (51.6720400, 39.1843000, 3)
-    coords['выездная'] = (0, 0, 0)
-    coords['жуковский'] = (55.5952800, 38.1202800, 1)
-    coords['интернет-магазин'] = (0, 0, 0)
-    coords['казань'] = (55.7887400, 49.1221400, 4)
-    coords['калуга'] = (54.5293000, 36.2754200, 4)
-    coords['коломна'] = (55.0794400, 38.7783300, 4)
-    coords['красноярск'] = (56.0183900, 92.8671700, 4)
-    coords['курск'] = (51.7373300, 36.1873500, 3)
-    coords['москва'] = (55.7522200, 37.6155600, 1)
-    coords['мытищи'] = (55.9116300, 37.7307600, 1)
-    coords['н.новгород'] = (56.3286700, 44.0020500, 4)
-    coords['новосибирск'] = (55.0415000, 82.9346000, 4)
-    coords['омск'] = (54.9924400, 73.3685900, 4)
-    coords['ростовнадону'] = (47.2313500, 39.7232800, 3)
-    coords['спб'] = (59.9386300, 30.3141300, 2)
-    coords['самара'] = (53.2000700, 50.1500000, 4)
-    coords['сергиев'] = (56.3000000, 38.1333300, 4)
-    coords['сургут'] = (61.2500000, 73.4166700, 4)
-    coords['томск'] = (56.4977100, 84.9743700, 4)
-    coords['тюмень'] = (57.1522200, 65.5272200, 4)
-    coords['уфа'] = (54.7430600, 55.9677900, 4)
-    coords['химки'] = (55.8970400, 37.4296900, 1)
-    coords['цифровой'] = (0, 0, 0)
-    coords['чехов'] = (55.1477000, 37.4772800, 4)
-    coords['ярославль'] = (57.6298700, 39.8736800, 2)
-    df['city_coord_1'] = df['city_name'].apply(lambda x: coords[x][0])
-    df['city_coord_2'] = df['city_name'].apply(lambda x: coords[x][1])
-    df['country_part'] = df['city_name'].apply(lambda x: coords[x][2])
+    coords["якутск"] = (62.028098, 129.732555, 4)
+    coords["адыгея"] = (44.609764, 40.100516, 3)
+    coords["балашиха"] = (55.8094500, 37.9580600, 1)
+    coords["волжский"] = (53.4305800, 50.1190000, 3)
+    coords["вологда"] = (59.2239000, 39.8839800, 2)
+    coords["воронеж"] = (51.6720400, 39.1843000, 3)
+    coords["выездная"] = (0, 0, 0)
+    coords["жуковский"] = (55.5952800, 38.1202800, 1)
+    coords["интернет-магазин"] = (0, 0, 0)
+    coords["казань"] = (55.7887400, 49.1221400, 4)
+    coords["калуга"] = (54.5293000, 36.2754200, 4)
+    coords["коломна"] = (55.0794400, 38.7783300, 4)
+    coords["красноярск"] = (56.0183900, 92.8671700, 4)
+    coords["курск"] = (51.7373300, 36.1873500, 3)
+    coords["москва"] = (55.7522200, 37.6155600, 1)
+    coords["мытищи"] = (55.9116300, 37.7307600, 1)
+    coords["н.новгород"] = (56.3286700, 44.0020500, 4)
+    coords["новосибирск"] = (55.0415000, 82.9346000, 4)
+    coords["омск"] = (54.9924400, 73.3685900, 4)
+    coords["ростовнадону"] = (47.2313500, 39.7232800, 3)
+    coords["спб"] = (59.9386300, 30.3141300, 2)
+    coords["самара"] = (53.2000700, 50.1500000, 4)
+    coords["сергиев"] = (56.3000000, 38.1333300, 4)
+    coords["сургут"] = (61.2500000, 73.4166700, 4)
+    coords["томск"] = (56.4977100, 84.9743700, 4)
+    coords["тюмень"] = (57.1522200, 65.5272200, 4)
+    coords["уфа"] = (54.7430600, 55.9677900, 4)
+    coords["химки"] = (55.8970400, 37.4296900, 1)
+    coords["цифровой"] = (0, 0, 0)
+    coords["чехов"] = (55.1477000, 37.4772800, 4)
+    coords["ярославль"] = (57.6298700, 39.8736800, 2)
+
+    df["city_coord_1"] = df["city_name"].apply(lambda x: coords[x][0])
+    df["city_coord_2"] = df["city_name"].apply(lambda x: coords[x][1])
+    df["country_part"] = df["city_name"].apply(lambda x: coords[x][2])
 
     return df
 
