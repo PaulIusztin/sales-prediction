27ab185040541a79ac119ad9355fe9adcfb58984
Paul Iusztin <p.e.iusztin@gmail.com>
2022-04-18 16:13:02 +0300

    Add lag features.

--- notebooks/data.py
+++ notebooks/data.py
@@ -1,5 +1,6 @@
+import datetime
 import os.path
-from typing import Tuple, List, Union, Dict
+from typing import Tuple, List, Union, Dict, Iterable
 
 import numpy as np
 import pandas as pd
@@ -18,8 +19,6 @@ def load_data(path_dir: str = "../data") -> Tuple[pd.DataFrame, pd.DataFrame]:
     train_df = pd.read_csv(os.path.join(path_dir, "sales_train.csv"))
     train_df = train_df.merge(items_df, on="item_id", how="left")
     train_df = train_df.merge(shops, on="shop_id", how="left")
-    train_df["date"] = pd.to_datetime(train_df["date"], format="%d.%m.%Y")
-    train_df = train_df.sort_values(by=["date"])
 
     test_df = pd.read_csv(os.path.join(path_dir, "test.csv"))
     test_df = test_df.merge(items_df, on="item_id", how="left")
@@ -31,15 +30,32 @@ def load_data(path_dir: str = "../data") -> Tuple[pd.DataFrame, pd.DataFrame]:
 
 def clean(df: pd.DataFrame) -> pd.DataFrame:
     """
-    :param df: dataframe that will have its NaN values filled, useless columns dropped and outliers removed.
+    :param df: dataframe that will have
+        * its NaN values filled
+        * useless columns dropped
+        * columns cast to the right type
+        * outliers removed
+        * other custom cleaning
     :return: cleaned dataframe
     """
 
-    # Data does not have any NaN values.
-    # TODO: Should we keep all the columns?
+    assert df.isna().sum().sum() == 0, "We expect a dataframe without NaN values."
 
+    df = cast_columns(df)
+    df = df.sort_values(by=["date"])
     df = aggregate_twin_transactions(df)
     df = remove_outliers_iqr(df, columns=["item_price", "item_cnt_day"])
+
+    return df
+
+
+def cast_columns(df: pd.DataFrame) -> pd.DataFrame:
+    df["date"] = pd.to_datetime(df["date"], format="%d.%m.%Y")
+    df["date_block_num"] = df["date_block_num"].astype(np.int8)
+    df["shop_id"] = df["shop_id"].astype(np.int8)
+    df["item_id"] = df["item_id"].astype(np.int16)
+    df["item_cnt_day"] = df["item_cnt_day"].astype(np.int16)
+    df["item_price"] = df["item_price"].astype(np.float32)
 
     return df
 
@@ -82,7 +98,8 @@ def add_features(df: pd.DataFrame, features: Dict[str, Union[bool, dict]]) -> pd
         "revenue": _add_item_revenue,
         "is_new_item": _add_is_new_item_feature,
         "is_first_shop_transaction": _add_first_shop_transaction_feature,
-        "city": _add_city_features
+        "city": _add_city_features,
+        "daily_lags": _add_daily_lags,
     }
     assert set(features.keys()).issubset(set(feature_functions.keys()))
 
@@ -172,21 +189,44 @@ def _add_city_features(df: pd.DataFrame) -> pd.DataFrame:
 def _add_time_features(df: pd.DataFrame) -> pd.DataFrame:
     df["day_of_year"] = df["date"].apply(lambda date: date.day_of_year)
     df["day_of_month"] = df["date"].apply(lambda date: date.day)
+    df["days_in_month"] = df["date"].apply(lambda date: date.days_in_month)
     # Everything starts from 1, except day_of_week. Map it to [1-7] for consistency.
     df["day_of_week"] = df["date"].apply(lambda date: date.day_of_week + 1)
     df["week_of_year"] = df["date"].apply(lambda date: date.weekofyear)
     df["month_of_year"] = df["date"].apply(lambda date: date.month)
 
-    df["days_in_month"] = df["date"].apply(lambda date: date.days_in_month)
     df["is_week_start"] = df["date"].apply(lambda date: (date.day_of_week + 1) == 1)
     df["is_week_end"] = df["date"].apply(lambda date: (date.day_of_week + 1) == 7)
     df["is_month_start"] = df["date"].apply(lambda date: date.is_month_start)
     df["is_month_end"] = df["date"].apply(lambda date: date.is_month_end)
     df["is_year_start"] = df["date"].apply(lambda date: date.is_year_start)
     df["is_year_end"] = df["date"].apply(lambda date: date.is_year_end)
-
     df["is_business_day"] = df["date"].apply(lambda date: is_business_day(date))
     df["is_holiday"] = df["date"].apply(lambda date: is_holiday(date))
+
+    df["is_week_start"] = df["is_week_start"].astype("int8")
+    df["is_week_end"] = df["is_week_end"].astype("int8")
+    df["is_month_start"] = df["is_month_start"].astype("int8")
+    df["is_month_end"] = df["is_month_end"].astype("int8")
+    df["is_year_start"] = df["is_year_start"].astype("int8")
+    df["is_year_end"] = df["is_year_end"].astype("int8")
+    df["is_business_day"] = df["is_business_day"].astype("int8")
+    df["is_holiday"] = df["is_holiday"].astype("int8")
+
+    return df
+
+
+def _add_daily_lags(df: pd.DataFrame, lags: List[int], column: str = "item_cnt_day", fill_value: float = 0):
+    for lag in lags:
+        lagged_df = df[["date", "shop_id", "item_id", column]].copy()
+        lagged_df.columns = ["date", "shop_id", "item_id", f"{column}_lag_{lag}"]
+        lagged_df["date"] = lagged_df["date"] + datetime.timedelta(days=lag)
+
+        df = df.merge(lagged_df, how="left", on=["date", "shop_id", "item_id"])
+        df = df.fillna(fill_value)
+
+        initial_dtype = df[column].dtype
+        df[f"{column}_lag_{lag}"] = df[f"{column}_lag_{lag}"].astype(initial_dtype)
 
     return df
 
@@ -197,7 +237,11 @@ if __name__ == "__main__":
     train_df = add_features(
         train_df,
         features={
-            "revenue": True
+            "revenue": True,
+            "daily_lags": {
+                "lags": [1, 30, 60, 365],
+                "fill_value": 0
+            }
         }
     )
     print(train_df)
